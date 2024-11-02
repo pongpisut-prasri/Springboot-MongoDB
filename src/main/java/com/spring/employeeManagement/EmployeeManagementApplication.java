cd4fea68b4c6af80b6b770e7e2ed09061376c1d4
mannieshub <ppmannies@gmail.com>
2024-11-03 01:45:22 +0700

    initial

---
+++ src/main/java/com/spring/employeeManagement/EmployeeManagementApplication.java
@@ -0,0 +1,15 @@
+package com.spring.employeeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/employeeManagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.employeeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
